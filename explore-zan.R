21cb0dbb5d01210dcb0382f726ac00d224b4cf45
j-rostand <jules.rostand@outlook.com>
2023-01-18 12:26:45 +0100

    Reprise du code, catégorisation manuelle, cartes et ne pas suivre QGIS

--- explore-zan.R
+++ explore-zan.R
@@ -2,38 +2,32 @@
 # Jules Rostand
 
 # Description
-# Ce script R propose une exploration dans les données de l'artificialisation des sols.
+# Ce script R propose une classification des EPCI selon l'artificialisation des sols et ses déterminants.
 
-# Librairies génériques
-library(readr)
-library(dplyr)
-library(ggplot2)
-
-# Statistiques descriptives
+# Bibliothèques
+library(tidyverse)
+library(textshape)
 library(pastecs)
-
-# Statistiques avancées
 library(FactoMineR)
 library(factoextra)
 library(questionr)
 library(explor)
-library(forcats)
 
-########################
-## Import des données ##
-########################
+#################################################
+## Préparation des données : artificialisation ##
+#################################################
 
 # Import des données de l'observatoire de l'artificialisation des sols
-
 read_delim(file = "data/obs_artif_conso_com_2009_2021.csv",
            delim = ";", 
            escape_double = FALSE,
            trim_ws = TRUE) -> artif
 
-## Calculs des flux de référence 2011-2021
+stat.desc(artif[73:77]) # 5 variables de flux d'artificialisation 2009-2021
 
-### Flux entre NAF et artificialisé sur la période 2011-2021
+# Calculs des flux de référence 2011-2021
 
+# Flux entre NAF et artificialisé sur la période 2011-2021
 artif$nafart1121 = artif$naf11art12 + artif$naf12art13 + artif$naf13art14 + 
   artif$naf14art15 + artif$naf15art16 + artif$naf16art17 + artif$naf17art18 + 
   artif$naf18art19 + artif$naf19art20 + artif$naf19art20 + artif$naf20art21
@@ -58,8 +52,7 @@ artif$artinc1121 = artif$art11inc12 + artif$art12inc13 + artif$art13inc14 +
   artif$art14inc15 + artif$art15inc16 + artif$art16inc17 + artif$art17inc18 + 
   artif$art18inc19 + artif$art19inc20 + artif$art19inc20 + artif$art20inc21
 
-## Regroupement par EPCI
-
+# Regroupement par EPCI
 artif %>%
   group_by(epci21, epci21txt) %>%
   summarise(nafart1121_tot = sum(nafart1121), 
@@ -70,17 +63,12 @@ artif %>%
             artcom2020_tot = sum(artcom2020 * surfcom2021) / sum(surfcom2021),
             surfcom2021_tot = sum(surfcom2021)) -> artif.epci
 
-# Import des données démographiques de l'Insee
-# TODO : à refaire avec les données de l'Insee reprises par l'ANCT, qui l'a réalisée à l'échelle bassin de vie
+###########################################
+## Préparation des données : démographie ##
+###########################################
+
+# Import des données démographiques de l'Insee, mises à disposition par l'Observatoire des Territoires de l'ANCT
 # https://www.observatoire-des-territoires.gouv.fr/visiotheque/2017-dynpop-typologie-de-levolution-de-la-population-entre-1999-et-2013
-
-#read_delim(file = "data/data_insee_2013-2019.csv",
-#           delim = ";", 
-#           escape_double = FALSE,
-#           skip = 2,
-#           trim_ws = TRUE,
-#           col_types = "cccc") -> pop1319
-
 readxl::read_xlsx(path = "data/insee_rp_evol_1968_var_pop.xlsx", skip = 4) -> insee.varpop
 readxl::read_xlsx(path = "data/insee_rp_evol_1968_sn_brut.xlsx", skip = 4) -> insee.soldenat
 readxl::read_xlsx(path = "data/insee_rp_evol_1968_sm_brut.xlsx", skip = 4) -> insee.soldemig
@@ -89,6 +77,7 @@ insee.varpop %>%
   left_join(insee.soldenat) %>%
   left_join(insee.soldemig) -> insee.poptot
 
+# Filtre sur la période 2008-2018 pour les données démographiques
 insee.poptot %>%
   filter(an == "2008-2013" | an == "2013-2018") %>%
   group_by(codgeo, libgeo) %>%
@@ -96,70 +85,75 @@ insee.poptot %>%
             sn_brut0818 = sum(sn_brut), 
             sm_brut0818 = sum(sm_brut)) %>%
   rename("epci21" = codgeo, "epci21txt" = libgeo) %>%
-  na.omit()-> insee.poptot0818
+  na.omit() -> insee.poptot0818
 
-#pop1319 %>%
-#  rename("epci21" = Code, "epci21insee" = Libellé, 
-#         "soldeapp1319" = `Évol. annuelle moy. de la pop. due au solde apparent entrées/sorties 2013-2019`,
-#         "soldenat1319" = `Évol. annuelle moy. de la pop. due au solde naturel 2013-2019`) %>%
-#  right_join(artif.epci, by = "epci21") %>%
-#  relocate(epci21, epci21insee, epci21txt) %>%
-#  mutate(across(soldeapp1319, as.numeric)) %>%
-#  mutate(across(soldenat1319, as.numeric)) %>%
-#  na.omit() -> artpop.epci
-
+# Tableau de données complet (démographie et artificialisation)
 artif.epci %>%
   left_join(insee.poptot0818, by = "epci21") %>%
   relocate(epci21, epci21txt.x, epci21txt.y, varpop0818, sn_brut0818, sm_brut0818, nafart1121_tot, 
            artact1121_tot, arthab1121_tot, artmix1121_tot, artinc1121_tot, artcom2020_tot, surfcom2021_tot) %>%
   mutate(across(surfcom2021_tot, as.numeric)) %>%
   ungroup() %>%
-  filter_all(all_vars(!is.na(.)))-> artpop.epci
-  
-# Transformaition en classes (icut)
+  filter_all(all_vars(!is.na(.))) -> artpop.epci
 
-#artpop.epci$varpop0818 = cut(artpop.epci$varpop0818, breaks=quantile(artpop.epci$varpop0818), labels = FALSE)
-#artpop.epci$sn_brut0818 = cut(artpop.epci$sn_brut0818, breaks=quantile(artpop.epci$sn_brut0818), labels = FALSE)
-#artpop.epci$sm_brut0818 = cut(artpop.epci$sm_brut0818, breaks=quantile(artpop.epci$sm_brut0818), labels = FALSE)
-#artpop.epci$nafart1121_tot = cut(artpop.epci$nafart1121_tot, breaks=quantile(artpop.epci$nafart1121_tot), labels = FALSE)
-#artpop.epci$artact1121_tot = cut(artpop.epci$artact1121_tot, breaks=quantile(artpop.epci$artact1121_tot), labels = FALSE)
-#artpop.epci$arthab1121_tot = cut(artpop.epci$arthab1121_tot, breaks=quantile(artpop.epci$arthab1121_tot), labels = FALSE)
-#artpop.epci$artmix1121_tot = cut(artpop.epci$artmix1121_tot, breaks=quantile(artpop.epci$artmix1121_tot), labels = FALSE)
-#artpop.epci$artinc1121_tot = cut(artpop.epci$artinc1121_tot, breaks=quantile(artpop.epci$artinc1121_tot), labels = FALSE)
-#artpop.epci$surfcom2021_tot = cut(artpop.epci$surfcom2021_tot, breaks=quantile(artpop.epci$surfcom2021_tot), labels = FALSE)
+##########################
+## Analyse des données ###
+##########################
 
-# ACM
-
-#artpop.epci %>%
-#  select(-epci21, -epci21txt.x, -epci21txt.y) %>%
-#  filter_all(all_vars(!is.na(.))) %>%
-#  MCA(ncp = 5, quanti.sup = 5:9, graph = FALSE, na.method = ) -> res.mca
+# Analyses en composantes principales sur l'artificialisation
 
 artpop.epci %>%
-  select(nafart1121_tot, surfcom2021_tot, artcom2020_tot) %>%
+  column_to_rownames("epci21") %>%
+  select(nafart1121_tot, artcom2020_tot) %>%
   filter_all(all_vars(!is.na(.))) %>%
   PCA(ncp = 5, graph = FALSE) -> res.pca
 
+# explor(res.pca)
+
 res.hcpc = HCPC(res.pca)
+res.hcpc$data.clust -> artif.map
+artif.map %>%
+  left_join(artif.epci) %>%
+  select(epci21, epci21txt, clust, nafart1121_tot, artcom2020_tot) %>%
+  write_excel_csv("res/artif-epci-map.csv", quote = "all")
+  
+# Analyses en composantes principales sur la démographie
 
-print(res.pca)
-barplot(res.pca$eig[, 2], main="Histogramme des valeurs propres", names.arg=rownames(res.pca$eig), xlab="Axes", ylab="Pourcentage d’inertie", cex.axis=0.8, font.lab=3, col="orange")
+artpop.epci %>%  
+  filter(epci21 != "200054781") %>%
+  column_to_rownames("epci21") %>%
+  select(varpop0818, sn_brut0818, sm_brut0818) %>%
+  filter_all(all_vars(!is.na(.))) %>%
+  PCA(ncp = 5, graph = FALSE) -> res2.pca
 
-explor(res.pca)
+explor(res2.pca)
 
-fviz_dend(res.hcpc, 
-          cex = 0.7,                     # Taille du text
-          palette = "jco",               # Palette de couleur ?ggpubr::ggpar
-          rect = TRUE, rect_fill = TRUE, # Rectangle autour des groupes
-          rect_border = "jco",           # Couleur du rectangle
-          labels_track_height = 0.8      # Augment l'espace pour le texte
-)
+res2.hcpc = HCPC(res2.pca)
+res2.hcpc$data.clust -> demographie.map
+demographie.map %>%
+  rownames_to_column("epci21") %>%
+  left_join(artif.epci, by = "epci21") %>%
+  select(epci21, epci21txt, clust, varpop0818, sn_brut0818, sm_brut0818) %>%
+  write_excel_csv("res/demographie-epci-map.csv", quote = "all")
 
-fviz_cluster(res.hcpc,
-             repel = TRUE,            # Evite le chevauchement des textes
-             show.clust.cent = TRUE, # Montre le centre des clusters
-             palette = "jco",         # Palette de couleurs, voir ?ggpubr::ggpar
-             ggtheme = theme_minimal(),
-             main = "Factor map"
-)
+# Classification manuelle pour la démographie
+# https://www.observatoire-des-territoires.gouv.fr/typologie-des-soldes-naturel-et-migratoire-apparent
+
+insee.poptot0818 %>%
+  mutate(varpop0818_rec = case_when(varpop0818 < 0 ~ 0, varpop0818 >= 0 ~ 1)) %>%
+  mutate(sn_brut0818_rec = case_when(sn_brut0818 < 0 ~ 0, sn_brut0818 >= 0 ~ 1)) %>%
+  mutate(sm_brut0818_rec= case_when(sm_brut0818 < 0 ~ 0, sm_brut0818 >= 0 ~ 1)) %>%
+  select(epci21, epci21txt, varpop0818_rec, sn_brut0818_rec, sm_brut0818_rec) -> insee.poptot0818.rec
+
+insee.poptot0818.rec %>%
+  mutate(clust = case_when(varpop0818_rec == 1 & sn_brut0818_rec == 1 & sm_brut0818_rec == 1 ~ "Croissance totale",
+                           varpop0818_rec == 1 & sn_brut0818_rec == 1 & sm_brut0818_rec == 0 ~ "Croissance liée à un solde naturel positif",
+                           varpop0818_rec == 1 & sn_brut0818_rec == 0 & sm_brut0818_rec == 1 ~ "Croissance liée à un solde migratoire apparent positif",
+                           varpop0818_rec == 0 & sn_brut0818_rec == 1 & sm_brut0818_rec == 0 ~ "Décroissance liée à un solde migratoire apparent négatif",
+                           varpop0818_rec == 0 & sn_brut0818_rec == 0 & sm_brut0818_rec == 1 ~ "Décroissance liée à un solde naturel négatif",
+                           varpop0818_rec == 0 & sn_brut0818_rec == 0 & sm_brut0818_rec == 0 ~ "Décroissance totale")) %>%
+  select(epci21, epci21txt, clust) -> insee.poptot0818.rec.clust
+
+insee.poptot0818.rec.clust %>%
+  write_excel_csv("res/demographie-epci-binaire-map.csv", quote = "all")
 
