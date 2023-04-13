f37a821b64641c664dd133ea01cc8090cf933828
j-rostand <jules.rostand@outlook.com>
2023-04-13 15:29:37 +0200

    correction de l'affichage des variables, tableau de synthèse

--- artificialisation-densite-communes.R
+++ artificialisation-densite-communes.R
@@ -25,10 +25,11 @@ read_csv(file = "res/artificialisation-communes-2011-2021.csv",
 insee_grille_densite_2023 %>%
   left_join(artificialisation_communes_2021, by = c("depcom" = "idcom")) %>%
   left_join(insee_recensement_2018, by = c("depcom" = "CODGEO")) %>%
-  mutate(menhab0818 = case_when(art09hab21 == 0 ~ 0, TRUE ~ (P18_RP - P08_RP)/(art09hab21/10000)),
-         menages_tcam = ((P18_RP/P08_RP)^(1/10) - 1)) %>%
+  mutate(menhab0818 = case_when(arthab1121 == 0 ~ 0, TRUE ~ (P18_RP - P08_RP)/(arthab1121/10000)),
+         menages_tcam = ((P18_RP/P08_RP)^(1/10) - 1),
+         parthab = arthab1121 / nafart1121) %>%
   select(depcom, libcom, libdens, artcom2020, nafart1121, 
-         arthab1121, artact1121, menhab1318, menhab0818, menages_tcam) -> territoires1
+         arthab1121, parthab, menhab1318, menhab0818, menages_tcam) -> territoires1
 
 # Factorisation
 territoires1$libdens <- as.factor(territoires1$libdens)
@@ -42,13 +43,13 @@ territoires1$libdens <- factor(territoires1$libdens, levels = c("Rural à habita
 
 # Statistiques descriptive sur les variables, utiles pour vérifier la déformation liée à la
 # représentation graphique
-tapply(territoires1$menhab0818, territoires1$libdens, FUN = summary) 
+tapply(territoires1$artcom2020, territoires1$libdens, FUN = summary) 
 
 # Surface communale artificialisé sur la période
 territoires1 %>% 
   ggplot(aes(x = libdens, y=nafart1121/10000)) + 
   geom_boxplot(notch = FALSE, outlier.shape = NA) +
-  scale_y_continuous(limits=c(0, 100)) +
+  coord_cartesian(ylim=c(0, 100)) + 
   stat_summary(fun.y=mean, geom="point", shape=23) +
   theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1),
         axis.title.y = element_text(size = 8),
@@ -60,27 +61,20 @@ territoires1 %>%
 territoires1 %>% 
   ggplot(aes(x = libdens, y=artcom2020)) + 
   geom_boxplot(notch = FALSE, outlier.shape = NA) +
-  scale_y_continuous(limits=c(0, 10)) +
-  stat_summary(fun.y=mean, geom="point", shape=23) +
+  coord_cartesian(ylim=c(0, 5)) + 
+  stat_summary(fun = "mean", geom="point", shape=23) +
   theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1),
         axis.title.y = element_text(size = 8),
         plot.title = element_text(size=10, hjust = 0.5, face="bold")) +
   labs(title="Part de la surface artificialisée en 2020 en fonction de la densité communale",
        x ="", y = "en %") 
 
-# Surface communale artificialisé pour l'habitat
-territoires1 %>% 
-  ggplot(aes(x = libdens, y=arthab1121/10000)) + 
-  geom_boxplot(notch = FALSE, outlier.shape = NA) +
-  scale_y_continuous(limits=c(0, 100)) +
-  stat_summary(fun.y=mean, geom="point", shape=23) +
-  theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1))
-
 # Nombre moyen de ménages par hectares artificialisé 
 territoires1 %>% 
   ggplot(aes(x = libdens, y=log10(menhab0818))) + 
     geom_boxplot(notch = FALSE, outlier.shape = NA) +
     stat_summary(fun.y=mean, geom="point", shape=23) +
+    coord_cartesian(ylim=c(0, 3)) + 
     theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1),
           axis.title.y = element_text(size = 8),
           plot.title = element_text(size=10, hjust = 0.5, face="bold")) +
@@ -91,7 +85,7 @@ territoires1 %>%
 territoires1 %>% 
   ggplot(aes(x = libdens, y=menages_tcam)) + 
   geom_boxplot(notch = FALSE, outlier.shape = NA) +
-  scale_y_continuous(limits=c(-0.025, 0.05)) +
+  coord_cartesian(ylim=c(-0.02, 0.04)) + 
   stat_summary(fun.y=mean, geom="point", shape=23) +
   theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1),
         axis.title.y = element_text(size = 8),
@@ -99,19 +93,22 @@ territoires1 %>%
   labs(title="Taux de croissance annuel moyen du nombre de ménages sur la période 2008_2018, en fonction de la densité communale",
        x ="", y = "en %") 
 
-# Comparaison entre la surface consommée et son efficacité
+# Moyennes des variables en fonction de la densité
 
 territoires1 %>%
   droplevels() %>%
   drop_na() %>%
   group_by(libdens) %>%
-    summarise(hab = log10(mean(arthab1121)),
-              men = log10(mean(menhab0818))) %>%
-  ggplot(aes(x = libdens)) +
-    geom_point(aes(y = hab), shape = 23) +
-    geom_point(aes(y = men), shape = 2) +
-    theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1),
-        axis.title.y = element_text(size = 8),
-        plot.title = element_text(size=10, hjust = 0.5, face="bold")) +
-    labs(title="Artificialisation pour l'habitat, en surface et en surface par ménages, sur la période 2008-2018, en fonction de la densité communale",
-       x ="", y = "log10") 
+  summarise(nombre = n(),
+            nafart1121_mean = mean(nafart1121),
+            arthab1121_mean = mean(arthab1121),
+            artcom2020_mean = mean(artcom2020),
+            parthab_mean = mean(parthab),
+            menages_tcam_mean = mean(menages_tcam),
+            menhab0818_mean = mean(menhab0818)) %>%
+  mutate(across(2:4, round, 1)) %>%
+  mutate(across(5:7, round, 4)) %>%
+  mutate(across(8:8, round, 1)) %>%
+  write.csv(file = "res/artificialisation_densite_communes_moyenne.csv")
+
+  
